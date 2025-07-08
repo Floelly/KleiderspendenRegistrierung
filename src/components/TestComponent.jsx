d9c3e69fe4b0f2f4042d1c9fd0b7cdd41c9752ce
Floelly <email@florianbowitz.de>
2025-07-08 11:01:03 +0200

    added test for theme and GlobalStyle

---
+++ src/components/TestComponent.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Box = styled.div`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.light};
+  padding: 1rem;
+`;
+
+export default function TestComponent() {
+  return <Box data-testid="test-box">Themed Box</Box>;
+}
---
+++ src/styles/GlobalStyle.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+import TestComponent from '../components/TestComponent'
+import { Theme as theme } from './Theme'
+import GlobalStyle from './GlobalStyle'
+import '@testing-library/jest-dom'
+
+describe('Theme + GlobalStyle', () => {
+  it('renders react TestComponent with theme styles', () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <TestComponent />
+      </ThemeProvider>
+    )
+
+    const box = screen.getByTestId('test-box')
+    expect(box).toHaveTextContent('Themed Box')
+    expect(box).toHaveStyle(`background-color: ${theme.colors.primary}`)
+    expect(box).toHaveStyle(`color: ${theme.colors.light}`)
+  })
+})
