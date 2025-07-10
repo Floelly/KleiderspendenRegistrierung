3083d63f5581863b94007ceca1d4600808513e15
Floelly <email@florianbowitz.de>
2025-07-10 23:44:19 +0200

    minimal style tweaks on inputs and selects

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -4,12 +4,11 @@ const Input = styled.input`
   font-size: ${({ theme }) => theme.fontSizes.body};
   padding: ${({ theme }) => theme.spacing.s} ${({ theme }) => theme.spacing.m};
   border: 1px solid;
-  border-radius: 4px;
   box-shadow: inset 1px 1px 3px rgba(0, 0, 0, 0.1);
   transition: border-color 0.3s ease;
   outline-offset: 2px;
 
-  &:focus {
+  &:focus, &:hover {
     border-color: ${({ theme }) => theme.colors.primary};
     outline: 2px solid ${({ theme }) => theme.colors.primaryLight};
   }
--- src/components/ui/Select.jsx
+++ src/components/ui/Select.jsx
@@ -4,13 +4,12 @@ const Select = styled.select`
   font-size: ${({ theme }) => theme.fontSizes.body};
   padding: ${({ theme }) => theme.spacing.s} ${({ theme }) => theme.spacing.m};
   border: 1px solid;
-  border-radius: 4px;
-  background: white;
+  background: ${({theme}) => theme.colors.light};
   box-shadow: inset 1px 1px 3px rgba(0, 0, 0, 0.1);
   transition: border-color 0.3s ease;
   outline-offset: 2px;
 
-  &:focus {
+  &:focus, &:hover {
     border-color: ${({ theme }) => theme.colors.primary};
     outline: 2px solid ${({ theme }) => theme.colors.primaryLight};
   }
