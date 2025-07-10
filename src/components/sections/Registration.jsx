860fef38954c7c114ca6a1cbd276b79d7b7303d7
Floelly <email@florianbowitz.de>
2025-07-10 16:01:06 +0200

    added useState for Registration and Confirmation in Kleider-Spenden-Section

--- src/components/sections/Registration.jsx
+++ src/components/sections/Registration.jsx
@@ -1,11 +1,21 @@
+import styled from "styled-components";
 import Section from "../ui/Section";
+import RegistrationForm from "../forms/RegistrationForm.jsx";
+import { useState } from "react";
+
+const ConfirmRegistration = styled.h1`
+  
+`
 
 export default function Registration() {
+  const [submittedData, setSubmittedData] = useState(null);
 
   return (
     <>
       <Section id="registration">
-        <h1>Section Registration</h1>
+        {submittedData ? 
+          <ConfirmRegistration data={submittedData} onPointerDown={() => setSubmittedData(null)}>Registration Confirmation</ConfirmRegistration>
+        : <RegistrationForm onSuccess={setSubmittedData}/>}
       </Section>
     </>
   );
