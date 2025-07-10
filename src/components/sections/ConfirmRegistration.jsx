819abd83bea78cf80ff9c1e877a4cc828dbf24ce
Floelly <email@florianbowitz.de>
2025-07-10 19:20:34 +0200

    Styled Confirm Registration

--- src/components/sections/ConfirmRegistration.jsx
+++ src/components/sections/ConfirmRegistration.jsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+import Button from "../ui/Button";
+
+const ConfirmationGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: ${({theme}) => theme.spacing.m};
+    align-items: center;
+
+    & :nth-child(odd):not(button) {
+        text-align: right;
+    }
+`
+
 export default function Confirmation({ data, onNew }) {
   const {
     mode,
@@ -14,25 +28,43 @@ export default function Confirmation({ data, onNew }) {
 
   return (
     <section>
-      <h2>Spende registriert!</h2>
+      <h2 style={{textAlign: "center"}}>Kleiderspende registriert!</h2>
 
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li><strong>Art der Spende:</strong> {mode === 'handover' ? 'Übergabe an Geschäftsstelle' : 'Abholung'}</li>
-        {mode === 'pickup' && (
-          <>
-            <li><strong>Abholadresse:</strong> {street + " " + houseNumber}</li>
-            <li><strong>PLZ:</strong> {postalCode}</li>
-            {additionalInfo && <li><strong>Bemerkung:</strong> {additionalInfo}</li>}
-          </>
-        )}
-        <li><strong>Kategorie Kleidung:</strong> {clothes}</li>
-        <li><strong>Krisengebiet:</strong> {region}</li>
-        <li><strong>Datum:</strong> {ts.toLocaleDateString()}</li>
-        <li><strong>Uhrzeit:</strong> {ts.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</li>
-      </ul>
+        <ConfirmationGrid>
+            <strong>Art der Spende:</strong> 
+            <span>{mode}</span>
 
-      <button onClick={() => window.print()}>Drucken</button>
-      <button onClick={onNew}>Weitere Spende registrieren</button>
+            {postalCode && (
+            <>
+                <strong>Abholadresse:</strong> 
+                <span>{street + " " + houseNumber}</span>
+
+                <strong>PLZ:</strong> 
+                <span>{postalCode}</span>
+
+                {additionalInfo && 
+                <>
+                    <strong>Bemerkung:</strong> 
+                    <span>{additionalInfo}</span>
+                </>}
+            </>
+            )}
+
+            <strong>Kategorie der Kleidung:</strong> 
+            <span>{clothes}</span>
+
+            <strong>Krisengebiet:</strong> 
+            <span>{region}</span>
+
+            <strong>Datum:</strong> 
+            <span>{ts.toLocaleDateString()}</span>
+
+            <strong>Uhrzeit:</strong> 
+            <span>{ts.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
+
+            <Button onClick={() => window.print()}>Drucken</Button>
+            <Button onClick={onNew}>Weitere Spende registrieren</Button>
+        </ConfirmationGrid>
     </section>
   );
 }
