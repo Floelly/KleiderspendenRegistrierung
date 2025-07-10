f0e2a3e49918c832f7fb65a03dbbcbed8584b361
Floelly <email@florianbowitz.de>
2025-07-10 02:02:06 +0200

    upgraded about

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,23 +1,34 @@
 import Section from "../ui/Section";
 import RoundImage from "../ui/RoundImage";
 import aboutImage from "/src/assets/About_Kleiderspende.jpg";
+import styled from "styled-components";
 
 export default function About() {
+  const SideBySide = styled.div`
+    @media screen AND (min-width: ${({ theme }) => theme.breakpoints.l}) {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: ${({ theme }) => theme.spacing.l};
+    }
+  `;
 
   return (
     <>
       <Section>
-        <div>
-          <h2>Wohin mit gut erhaltener Kleidung? Wir kümmern uns darum – weltweit.</h2>
-          <p>
-            Du willst Platz schaffen im Kleiderschrank und gleichzeitig Gutes tun? Bei <strong>Fairstofft!</strong> aus Frankfurt kannst du ganz einfach deine Kleiderspende mit einem Wunschziel auf der ganzen Welt verbinden. Ob nach Ghana, Indien, Rumänien oder in die Ukraine – wir bringen deine Kleidung genau dorthin, wo sie gebraucht wird.
-          </p>
-          <p>
-            Egal ob du sie persönlich bei uns in Frankfurt vorbeibringst, sie per Post an uns versendest oder unseren kostenlosen Abholservice in deiner Nähe nutzt – der Weg deiner Spende ist einfach, transparent und sinnvoll.
-          </p>
-          <p><strong>Wähle dein Ziel. Wir machen den Rest.</strong></p>
-        </div>
-        <RoundImage src={aboutImage} alt="Foto Kleiderspende" title="Bildquelle: https://unsplash.com/photos/person-holding-white-clothes-hanger-aAVVmgod3N4" />
+        <SideBySide>
+          <div>
+            <h2>Wohin mit gut erhaltener Kleidung? Wir kümmern uns darum – weltweit.</h2>
+            <p>
+              Du willst Platz schaffen im Kleiderschrank und gleichzeitig Gutes tun? Bei <strong>Fairstofft!</strong> aus Frankfurt kannst du ganz einfach deine Kleiderspende mit einem Wunschziel auf der ganzen Welt verbinden. Ob nach Ghana, Indien, Rumänien oder in die Ukraine – wir bringen deine Kleidung genau dorthin, wo sie gebraucht wird.
+            </p>
+            <p>
+              Egal ob du sie persönlich bei uns in Frankfurt vorbeibringst, sie per Post an uns versendest oder unseren kostenlosen Abholservice in deiner Nähe nutzt – der Weg deiner Spende ist einfach, transparent und sinnvoll.
+            </p>
+            <p><strong>Wähle dein Ziel. Wir machen den Rest.</strong></p>
+          </div>
+          <RoundImage src={aboutImage} alt="Foto Kleiderspende" title="Bildquelle: https://unsplash.com/photos/person-holding-white-clothes-hanger-aAVVmgod3N4" />
+        </SideBySide>
       </Section>
     </>
   );
