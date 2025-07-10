71de230528149cda600197d7b2b747b3f7779da8
Floelly <email@florianbowitz.de>
2025-07-10 12:01:31 +0200

    added impressum and datenschutzerklärung content

---
+++ src/components/ui/FancyBordure.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const FancyBordure = styled.div`
+  width:100%;
+  height: 20vh;
+  background: url('src/assets/TitleImage1.jpg');
+  background-repeat: no-repeat;
+  overflow: hidden;
+`
+
+export default FancyBordure;
