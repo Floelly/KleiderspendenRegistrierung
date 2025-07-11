275cc95613106947abf6c2773b221bddb2236448
Floelly <email@florianbowitz.de>
2025-07-11 08:41:41 +0200

    path routing for easier imports

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,13 @@ export default defineConfig({
     setupFiles: './tests/setupTests.js',
     css: true,
   },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      layoutComponents: path.resolve(__dirname, 'src/components/layout'),
+      ui: path.resolve(__dirname, 'src/components/ui'),
+    }
+  }
 })
